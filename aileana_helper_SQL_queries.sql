fd88809fba1fae66a99c8c71827452ded2521636
DimKouts84 <dimitriskouts@gmail.com>
2024-10-02 21:30:44 +0300

    Test commit

---
+++ aileana_helper_SQL_queries.sql
@@ -0,0 +1,6 @@
+-- Update job_listings set imported = null;
+-- SELECT COUNT(*) FROM job_listings WHERE imported IS NULL;
+-- SELECT COUNT(*) FROM job_listings WHERE imported IS NOT NULL;
+SELECT * FROM job_listings WHERE reference = 'REF. NUM: 260364';
+
+
